d8fc5aec842a06c4611fc4eaf099f381f44fa240
Erset <jerome.p24@free.fr>
2014-02-11 17:19:14 +0100

    ajout du post dans le controller register

--- app/controllers/register.rb
+++ app/controllers/register.rb
@@ -10,9 +10,18 @@ module MyScrum
     # =============
     
     get '/' do
+      @user = User.new
       haml :register
     end
       
+    post '/' do
+      @user = User.new(params[:user])
+      @user.save
+      session[:user] = @user.pk
+      @current_user = @user
+      flash[:notice] = "Account created."
+      redirect '/'
+    end
 
   end
 end
