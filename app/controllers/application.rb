3203e2cccb8ba0e62458b2bcc29a2bf7a05e6c32
Largaroth <rya.herb@gmail.com>
2014-02-12 12:00:30 +0100

    moving things around, and adding 404 pages

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -33,6 +33,32 @@ module MyScrum
       haml :index
     end
 
+    get '/signup' do
+      @user = User.new
+      @turing_test = TuringTest.new
+      session[:turing_answer] = @turing_test.answer
+      haml :signup
+    end
+      
+    post '/users' do
+      @user = User.new(params[:user])
+      begin
+        throw Exception if params[:turing_answer] != session[:turing_answer]
+        @user.save
+        session[:user] = @user.pk
+        @current_user = @user
+        flash[:notice] = "Account created."
+        redirect '/'
+      rescue
+        @user.valid? if params[:turing_answer] != session[:turing_answer]
+        @errors = @user.errors
+        @errors[:turing_answer] = "Your answer is incorrect. Please try again" if params[:turing_answer] != session[:turing_answer]
+        @turing_test = TuringTest.new
+        session[:turing_answer] = @turing_test.answer
+        haml :signup
+      end
+    end
+
     # ===========
     # = The End =
     # ===========
