77bd44d0cafc346196a597455bee56ef55f017e2
Erset <jerome.p24@free.fr>
2014-02-12 10:58:56 +0100

    changement de la view register pour passer les params en post et ajouter le test de turing

--- app/controllers/register.rb
+++ app/controllers/register.rb
@@ -11,17 +11,28 @@ module MyScrum
     
     get '/' do
       @user = User.new
+      @turing_test = TuringTest.new
+      session[:turing_answer] = @turing_test.answer
       haml :register
     end
       
-    post '/' do
+    post '/users' do
       @user = User.new(params[:user])
-      @user.save
-      session[:user] = @user.pk
-      @current_user = @user
-      flash[:notice] = "Account created."
-      redirect '/'
+      begin
+        throw Exception if params[:turing_answer] != session[:turing_answer]
+        @user.save
+        session[:user] = @user.pk
+        @current_user = @user
+        flash[:notice] = "Account created."
+        redirect '/'
+      rescue
+        @user.valid? if params[:turing_answer] != session[:turing_answer]
+        @errors = @user.errors
+        @errors[:turing_answer] = "Your answer is incorrect. Please try again" if params[:turing_answer] != session[:turing_answer]
+        @turing_test = TuringTest.new
+        session[:turing_answer] = @turing_test.answer
+        haml :register
+      end
     end
-
   end
 end
