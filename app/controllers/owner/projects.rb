30157b48739efe295515b96fb1b4eb7a3d63c366
Erset <jerome.p24@free.fr>
2014-02-13 14:11:53 +0100

    added create and edit function for projects

--- app/controllers/owner/projects.rb
+++ app/controllers/owner/projects.rb
@@ -7,7 +7,7 @@ module MyScrum
     end
 
     get '/projects/:id/show' do |id|
-      @project = @current_owner.projects.inject(nil) do |n, p|
+      @project = @current_owner.projects.inject do |n, p|
         if p.id == id
           return p
         end
@@ -16,12 +16,37 @@ module MyScrum
     end
 
     get '/projects/:id/edit' do |id|
-      @project = @current_owner.projects.inject(nil) do |n, p|
+      @project = @current_owner.projects.inject do |n, p|
         if p.id == id
           return p
         end
       end
       haml :"/projects/edit"
+    end
+
+    get '/projects/create' do
+      @project = Project.new
+      #@project.set(params[:project])
+      #@project.save
+      haml :"/projects/edit"
+    end
+
+    post '/projects' do
+      @project = Project.new
+      @project.set(params[:project])
+      @project.save
+      redirect "/owner/projects"
+    end
+
+    put '/projects/:id' do |id|
+      @project = @current_owner.projects.inject do |n, p|
+        if p.id == id
+          return p
+        end
+      end
+      @project.set(params[:project])
+      @project.save
+      redirect '/owner/projects'
     end
 
   end
