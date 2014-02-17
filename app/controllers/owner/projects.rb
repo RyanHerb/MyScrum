fd00b58085216dcc2ffdc6751bdee4bb332d4de7
Erset <jerome.p24@free.fr>
2014-02-17 13:42:05 +0100

    we can now add a user to a project if the user isn't already a collaborator

--- app/controllers/owner/projects.rb
+++ app/controllers/owner/projects.rb
@@ -26,8 +26,38 @@ module MyScrum
         arr
       end
       @project = @project.first
-      @owners = Owner.all
+
+      @owners = Owner.all.inject([]) do |arr2, o|
+        unless o.projects.include?(@project)
+          arr2 << o
+        end
+        arr2
+      end
       haml :"/projects/users"
+    end
+
+    post '/projects/:id/users/add' do |i|
+      @project = @current_owner.projects.inject([]) do |arr, p|
+        if p.pk == i.to_i
+          arr << p
+        end
+        arr
+      end
+      @project = @project.first
+      @owner = Owner.all.inject([]) do |arr2, o|
+        if o.pk == params[:owner].to_i
+          arr2 << o
+        end
+        arr2
+      end
+      @owner = @owner.first
+
+      if @owner.valid?
+        @project.add_user(@owner)
+      else
+        puts "FAIL"
+      end
+      haml :"/projects/show"
     end
 
     get '/projects/:id/edit' do |i|
@@ -48,6 +78,7 @@ module MyScrum
 
     post '/projects' do
       @project = Project.new
+      @bite.set(params[:bite])
       @project.set(params[:project])
       if @project.valid?
         @project.save
