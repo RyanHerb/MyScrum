072f3b8000f3a506039aed4cd1e80eda1f53f6f2
tdiallo <tdiallo@debian>
2014-02-17 22:33:49 +0100

    mis à jour de la classe UserStory

---
+++ app/models/userstory.rb
@@ -0,0 +1,10 @@
+class UserStory < Sequel::Model
+  
+  	def validate
+    	validates_presence :title, :message => 'Title must not be blank'
+    	validates_unique :title
+
+    	validates_presence :description, :message => 'Description must not be blank'
+    	validates_unique :description  
+  	end
+end
