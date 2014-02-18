af9cd19b3575eaf8c545984313d805df69ff1b66
Largaroth <rya.herb@gmail.com>
2014-02-18 14:35:12 +0100

    refactoring user stories to comply with other urls

--- app/controllers/owner/user_stories.rb
+++ app/controllers/owner/user_stories.rb
@@ -1,49 +1,55 @@
 module MyScrum
 	class OwnerApp < Sinatra::Application
 
-		get '/user_stories/all' do
-      		@user_stories = UserStory.all
-      		haml :"/user_stories/index"
-    	end
+		get '/projects/:pid/user_stories/all' do |pid|
+      @project = Project.find(:id => pid)
+  		@user_stories = UserStory.all
+  		haml :"/user_stories/index"
+  	end
 
-		get '/user_stories/create' do
+		get '/projects/:pid/user_stories/create' do |pid|
+      @project = Project.find(:id => pid)
 			@user_story = UserStory.new
 			haml :"/user_stories/edit"
 		end
 
-		get '/user_stories/:id/edit' do |i|
-      		@user_story = UserStory.find(:id => i)
-      		haml :"/user_stories/edit"
-    	end
-
-  	put '/user_stories/:id' do |id|
-    		@user_story = Project.find(:id => id)
-    		@user_story.set(params[:userstory])
-    		if @userstory.valid?
-      		@user_story.save
-      		flash[:notice] = "Userstory updated"
-      		redirect '/owner/userstories/all'
-    		else
-      		haml :"/user_stories/edit"
-    		end
+		get '/projects/:pid/user_stories/:id/edit' do |pid, id|
+      @project = Project.find(:id => pid)
+  		@user_story = UserStory.find(:id => id)
+  		haml :"/user_stories/edit"
   	end
 
-		post '/user_stories' do
+  	put '/projects/:pid/user_stories/:id' do |pid, id|
+      @project = Project.find(:id => pid)
+  		@user_story = Project.find(:id => id)
+  		@user_story.set(params[:userstory])
+  		if @userstory.valid?
+    		@user_story.save
+    		flash[:notice] = "Userstory updated"
+    		redirect '/owner/userstories/all'
+  		else
+    		haml :"/user_stories/edit"
+  		end
+  	end
+
+		post '/projects/:pid/user_stories' do |pid|
+      @project = Project.find(:id => pid)
 		  @user_story = UserStory.new
       @user_story.set(params[:user_story])
       if @user_story.valid?
   	 		@user_story.save
   	 		flash[:notice] = "User Stories created"
-  	 		redirect '/owner/user_stories/all'
+  	 		redirect "/owner/projects/#{pid}/user_stories/all"
 		 	else
         flash[:notice] = "An error occured"
 		 		haml :"/user_stories/edit"
 		 	end
 		end
 
-		get '/user_stories/:id/show' do |i|
-      		@user_story = UserStory.find(:id => i)
-      		haml :"/user_stories/show"
-    	end
+		get '/projects/:pid/user_stories/:id/show' do |pid, id|
+      @project = Project.find(:id => pid)
+  		@user_story = UserStory.find(:id => i)
+  		haml :"/user_stories/show"
+  	end
 	end
 end
