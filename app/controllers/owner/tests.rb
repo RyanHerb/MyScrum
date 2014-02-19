1e81155ae40ae19a0a5606565099d64d105a6d0c
Erset <jerome.p24@free.fr>
2014-02-19 11:12:11 +0100

    added the test edition

--- app/controllers/owner/tests.rb
+++ app/controllers/owner/tests.rb
@@ -10,8 +10,33 @@ module MyScrum
       haml :"/tests/form"
     end
 
-    post '/projects/:id/test/create' do |i|
-      @project = Project.find(:id => i)
+    get '/projects/:id/tests/:tid/edit' do |pid, tid|
+      @project = Project.find(:id => pid)
+      @test = Test.find(:id => tid)
+      @owners = @project.users
+      @user_stories = @project.user_stories
+      haml :"/tests/form"
+    end
+
+    put '/tests/:tid' do |tid|
+      @us = UserStory.find(:id => params[:test][:user_story_id])
+      @project = Project.find(:id => @us.project_id)
+      @test = Test.find(:id => tid)
+      @owners = @project.users
+      @user_stories = @project.user_stories
+
+      @test.set(params[:test])
+      if @test.valid?
+        @test.save
+        redirect "owner/projects/#{@project.pk}/show"
+      else
+        haml :"/tests/form"
+      end
+    end
+
+    post '/tests' do
+      @us = UserStory.find(:id => params[:test][:user_story_id])
+      @project = Project.find(:id => @us.project_id)
       @test = Test.new
       @owners = @project.users
       @user_stories = @project.user_stories
