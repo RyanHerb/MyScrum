cf7b5394418d6ae9ac47f1ec14d8ca33ff9fa3cf
Largaroth <rya.herb@gmail.com>
2014-02-20 13:54:00 +0100

    renamed tasks association

--- app/models/user_story.rb
+++ app/models/user_story.rb
@@ -1,10 +1,13 @@
 class UserStory < Sequel::Model
-  one_to_many :tests
-  
-  	def validate
-    	validates_presence :title, :message => 'Title must not be blank'
-    	validates_unique :title
 
-    	validates_presence :description, :message => 'Description must not be blank'
-  	end
+  one_to_many :jobs
+  one_to_many :tests
+  many_to_one :project
+  
+    def validate
+      validates_presence :title, :message => 'Title must not be blank'
+      validates_unique :title
+
+      validates_presence :description, :message => 'Description must not be blank'
+    end
 end
