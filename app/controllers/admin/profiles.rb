58ad8f20ada1e0c0e02af84e4d90c6b6c9ddc12d
Largaroth <rya.herb@gmail.com>
2014-02-25 16:18:11 +0100

    adding admin management

---
+++ app/controllers/admin/profiles.rb
@@ -0,0 +1,47 @@
+module MyScrum
+  class AdminApp
+
+    get '/profiles' do
+      @profiles = Admin.all
+      haml :"profiles/list"
+    end
+
+    post '/profiles' do
+      @admin = Admin.new
+      @admin.set(params[:admin])
+      if @admin.valid?
+        @admin.save
+        redirect '/admin/profiles'
+      else
+        haml :"profiles/form"
+      end
+    end
+
+    get '/profiles/new' do
+      @admin = Admin.new
+      haml :"profiles/form"
+    end
+
+    put '/profiles/:id' do |id|
+      @admin = Admin.find(:id => id).first || halt(404)
+      @admin.set(params[:admin])
+      if @admin.valid?
+        @admin.save
+        redirect '/admin/profiles'
+      else
+        haml :"profiles/form"
+      end
+    end
+
+    get '/profiles/:id/show' do |id|
+      @admin = Admin.find(:id => id) || halt(404)
+      haml :"profiles/show"
+    end
+
+    get '/profiles/:id/edit' do |id|
+      @admin = Admin.find(:id => id) || halt(404)
+      haml :"profiles/form"
+    end
+
+  end
+end
