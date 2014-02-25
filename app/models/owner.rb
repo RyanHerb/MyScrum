ca4e982096c557daec47c93cff42b55e835bcea2
Largaroth <rya.herb@gmail.com>
2014-02-25 16:21:42 +0100

    moved username validation up to owner.rb from user.rb

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -10,22 +10,13 @@ class Owner < Sequel::Model
   many_to_many :projects, class: :Project , join_table: :users_projects, left_key: :user, right_key: :project, :select => [:projects.*, :users_projects__position]
   one_to_many :notifications
 
-  # =========
-  # = Hooks =
-  # =========
-  
-  def save_login
-    # Called from auth helpers
-    self.last_login_at = self.current_login_at
-    self.last_login_ip = self.current_login_ip
-    # TODO find out how wwe can take remote ip
-    # self.current_login_ip = env['REMOTE_ADDR'] 
-    self.current_login_at = Time.now
-    self.number_of_logins = self.number_of_logins + 1
-  end
-  
-  def save_login!
-    save_login
-    save :validate => false
+  # ==============
+  # = Validation =
+  # ==============
+
+  def validate
+    super
+    validates_presence :username, :message => 'Username must not be blank'
+    validates_unique :username
   end
 end
