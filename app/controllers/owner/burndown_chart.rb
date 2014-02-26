e3f4a44a71563d8b324305b2990aa566151e389a
tdiallo <tdiallo@debian>
2014-02-26 23:21:18 +0100

    amélioration de laffichage du burndown chart

--- app/controllers/owner/burndown_chart.rb
+++ app/controllers/owner/burndown_chart.rb
@@ -1,3 +1,7 @@
+require 'tempfile'
+require 'fileutils'
+require 'csv'
+
 module MyScrum
   class OwnerApp < Sinatra::Application
     
@@ -7,6 +11,20 @@ module MyScrum
     get '/projects/:pid/sprints/:sid/burndown_chart/show' do |pid, sid|
       @project = @current_owner.projects_dataset.where(:project => pid).first || halt(404)
       @sprint = Sprint.find(:id => sid)
+      @Users = Owner.all
+      sortie = Tempfile.new(['data', '.csv'])
+      @data_path = sortie.path
+
+      CSV.open(sortie.path, 'ab') do |csv|
+        csv << ["name","num_jobs"]
+        @Users.each do |row|   
+          h = {:name => row.username, :num_jobs => row.jobs.length}
+          csv << h.values
+        end
+      end
+
+      
+
       haml :"/burndown_chart/show"
     end
   end
