66b5ba279f2c92d34f32a6aa9a51e80bf4c61f78
Largaroth <rya.herb@gmail.com>
2014-02-26 17:19:34 +0100

    removed that pesky whitespace

--- app/models/user_story.rb
+++ app/models/user_story.rb
@@ -8,7 +8,6 @@ class UserStory < Sequel::Model
     def validate
       validates_presence :title, :message => 'Title must not be blank'
       validates_unique :title
-
       validates_presence :description, :message => 'Description must not be blank'
     end
 end
