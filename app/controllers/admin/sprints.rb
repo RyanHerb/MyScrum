3645c6693e55cf33c395a5b3e66d84274f615391
tdiallo <tdiallo@debian>
2014-03-06 00:33:44 +0100

    liste de tous les sprints ainsi que les pages d'edition

---
+++ app/controllers/admin/sprints.rb
@@ -0,0 +1,46 @@
+require 'date'
+module MyScrum
+  class AdminApp
+
+    get '/sprints' do
+      @sprints = Sprint.all
+      haml :"sprints/list"
+    end
+
+    get '/sprints/:sid/show' do |sid|
+      @sprint = Sprint.find(:id => sid)
+      @user_stories = @sprint.user_stories
+      @sprint_difficulty = 0
+      @user_stories.each do |u|
+        u.jobs.each do |j|
+          @sprint_difficulty = @sprint_difficulty + j.difficulty
+        end
+      end
+      haml :"/sprints/show"
+    end
+
+    get '/sprints/:sid/edit' do |j|
+      @sprint = Sprint.find(:id => j)
+      @project = @sprint.project
+      @user_stories = @sprint.user_stories
+      haml :"/sprints/form"
+    end
+
+    put '/sprints/:sid/edit' do |j|
+      @project = @sprint.project
+      @user_stories = params[:userstories]
+      @sprint = Sprint.find(:id => j)
+      @date = DateTime.new(params[:year].to_i, params[:month].to_i, params[:day].to_i)
+      @sprint.set(params[:sprint])
+      if @sprint.valid?
+        @sprint.save
+        @user_stories.each do |i|
+          @sprint.add_user_story(i)
+        end
+        redirect "admin/sprints/#{@sprint.pk}/show"
+      else
+        haml :"/sprints/form"
+      end
+    end
+  end
+end
