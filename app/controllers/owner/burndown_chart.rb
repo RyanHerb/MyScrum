e95ab4ad0d92a9d7a6ccdb3ec7f73e6f8fd4e8fb
tdiallo <tdiallo@debian>
2014-03-11 11:07:59 +0100

    fin de la tâche User Contributions Chart for Tasks Done

--- app/controllers/owner/burndown_chart.rb
+++ app/controllers/owner/burndown_chart.rb
@@ -1,6 +1,7 @@
 require 'tempfile'
 require 'fileutils'
 require 'csv'
+require 'json'
 
 module MyScrum
   class OwnerApp < Sinatra::Application
@@ -11,19 +12,25 @@ module MyScrum
     get '/projects/:pid/sprints/:sid/burndown_chart/show' do |pid, sid|
       @project = @current_owner.projects_dataset.where(:project => pid).first || halt(404)
       @sprint = Sprint.find(:id => sid)
+      @user_stories = @sprint.user_stories
+      @sprint_difficulty = 0
+      @user_stories.each do |u|
+        u.jobs.each do |j|
+          @sprint_difficulty += j.difficulty
+        end
+      end
       @Users = Owner.all
-      #sortie = Tempfile.new(['data', '.csv'],ROOT_DIR + '/public/data')
-      #@data_name = sortie.path.split("/").last
+      sortie = Tempfile.new(['data', '.csv'],ROOT_DIR + '/public/data')
+      @data_name = sortie.path.split("/").last
 
-      #CSV.open(sortie.path, 'ab') do |csv|
-      #  csv << ["name","num_jobs"]
-      #  @Users.each do |row|   
-      #    h = {:name => row.username, :num_jobs => row.jobs.length}
-      #    csv << h.values
-       # end
-      #end
+      CSV.open(sortie.path, 'ab') do |csv|
+        csv << ["name","num_jobs"]
+        @Users.each do |row|   
+          h = {:name => row.username, :num_jobs => row.jobs.length}
+          csv << h.values
+        end
+      end
       haml :"/burndown_chart/show"
     end
   end
 end
-
