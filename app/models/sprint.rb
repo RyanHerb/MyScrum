b0a56984bba919c766c06cd277f32dc2c7cc79a2
Largaroth <rya.herb@gmail.com>
2014-03-17 16:00:47 +0100

    postits now can't be edited when done

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -1,4 +1,6 @@
+require 'date'
 class Sprint < Sequel::Model
+
 
   many_to_one :project
   many_to_many :user_stories
@@ -6,4 +8,17 @@ class Sprint < Sequel::Model
     def validate
       validates_presence :duration, :message => 'You must select a valid duration'
     end 
+
+    # ===========
+    # = Subsets =
+    # ===========
+
+    # ====================
+    # = Instance Methods =
+    # ====================
+
+    def expired
+      start_date.to_date < Date.today - duration
+    end
 end
+  
