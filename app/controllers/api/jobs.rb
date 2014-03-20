d0d4a191f441876445f7ea026f815396280323e7
Largaroth <rya.herb@gmail.com>
2014-03-20 10:57:39 +0100

    refactored jobs fetching in order to have some easy sorting functionnality

--- app/controllers/api/jobs.rb
+++ app/controllers/api/jobs.rb
@@ -5,15 +5,31 @@ module MyScrum
       @project = @current_owner.projects_dataset.where(:project => pid).first || halt(404)
       @user_stories = @project.user_stories
 
-      @jobs = @user_stories.inject([]) do |a, u|
-        a << u.jobs
-        a.flatten
-      end
-      response = @jobs.inject([]) do |a, j|
-        a << j.to_json
+      @jobs = @user_stories.inject({}) do |a, u|
+        if a['all'].nil?
+          a['all'] = []
+        end
+        if a['done'].nil?
+          a['done'] = []
+        end
+        if a['todo'].nil? 
+          a['todo'] = []
+        end
+        if a['in_progress'].nil? 
+          a['in_progress'] = []
+        end
+        a['all'] << u.jobs.inject([]){|a, j| a << j.to_json; a}
+        a['done'] << u.jobs_dataset.done.all.inject([]){|a, j| a << j.to_json; a}
+        a['todo'] << u.jobs_dataset.todo.all.inject([]){|a, j| a << j.to_json; a}
+        a['in_progress'] << u.jobs_dataset.in_progress.all.inject([]){|a, j| a << j.to_json; a}
         a
       end
-      response.to_json
+      @jobs['all'].flatten!
+      @jobs['done'].flatten!
+      @jobs['todo'].flatten!
+      @jobs['in_progress'].flatten!
+
+      response = @jobs.to_json
     end
 
     get '/owner/projects/:pid/sprints/:sid/jobs/:state' do |pid, state|
